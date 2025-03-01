8fd754bce17d59937dca8c8d89f96a654b1405e8
Olympus <Zeus@Olympuss-MBP.lan>
2025-02-28 22:22:45 -0500

    2nd push

---
+++ 0-Auth.tf
@@ -0,0 +1,13 @@
+# Teraform homework part 1.
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
---
+++ 14-Output.tf
@@ -0,0 +1,35 @@
+# output "homework1-vpc-info" {
+#     value = aws_vpc.app1.tags
+# }
+
+# output "homework1-vpc-id" {
+#   value = aws_vpc.app1.id
+# }
+
+# output "homework1-vpc-cidr" {
+#   value = aws_vpc.app1.cidr_block
+# }
+
+# output "homework1-public-subnet-1-cidr" {
+#   value = aws_subnet.public-us-east-1a.cidr_block
+# }
+
+# output "homework1-public-subnet-2-cidr" {
+#   value = aws_subnet.public-us-east-1b.cidr_block
+# }
+
+# output "homework1-public-subnet-3-cidr" {
+#   value = aws_subnet.public-us-east-1c.cidr_block
+# }
+
+# output "homework1-private-subnet-1-cidr" {
+#   value = aws_subnet.private-us-east-1a.cidr_block
+# }
+
+# output "homework1-private-subnet-2-cidr" {
+#   value = aws_subnet.private-us-east-1b.cidr_block
+# }
+
+# output "homework1-private-subnet-3-cidr" {
+#   value = aws_subnet.private-us-east-1c.cidr_block
+# }
